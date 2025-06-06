983681e71fab59e48716363788c9b0c8be7e4ae8
Franky <wtf.franky05@gmail.com>
2025-06-06 01:43:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/FrankyOS-Terminal/', // ← troca pelo nome do teu repositório!
   plugins: [react()],
-});
+})
